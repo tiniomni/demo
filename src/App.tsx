bafd22f7b4b83969d95c2e2f89b3d9fbf1eb54ac
panningzhi <ningzhipan@link.cuhk.edu.cn>
2025-12-14 20:30:17 +0800

    fix Router

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/demo">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
