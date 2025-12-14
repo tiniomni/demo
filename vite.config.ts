07d91ff45913cbc9c3267be9aa4c49bbfba01fa7
panningzhi <ningzhipan@link.cuhk.edu.cn>
2025-12-14 20:06:03 +0800

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { viteSourceLocator } from '@metagptx/vite-plugin-source-locator';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/demo/' : '/',
+  base: '/demo/',
   plugins: [
     viteSourceLocator({
       prefix: 'mgx',
